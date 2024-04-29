c4df110d8045b02b4b782a4cfdd19544e4fdb8c0
simonakomarovska <simonak399@gmail.com>
2024-04-29 22:14:18 +0200

    fixed responsive

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const input = document.querySelector(".input")
-    const btn = document.querySelector(".btn")
+const input = document.querySelector("input")
+    const btn = document.querySelector("button")
     const result = document.querySelector(".result")
 
     btn.addEventListener("click", palindrom)
@@ -8,7 +8,7 @@ function palindrom () {
     let inputVal = input.value
     let lenght = inputVal.length
 
-    let start = inputVal.substring(0, Math.floor(lenght/2)).toLowerCase(); //zodzio pabaiga (sveikasis skaicius) Math.floor(len/2)
+    let start = inputVal.substring(0, Math.floor(lenght/2)).toLowerCase(); //end of word (Integer) Math.floor(len/2)
     console.log(start)
 
     let end = inputVal.substring(lenght-Math.floor(lenght/2))
@@ -23,20 +23,20 @@ function palindrom () {
     if (!inputVal) {
         result.innerHTML = `Don't forget to type your word!`
         result.style.display  = 'block';
-        result.style.backgroundColor = 'rgba(205, 142, 174, 0.618)';
+        result.style.backgroundColor = '#c69ab09e';
     }
     else if (!/^[A-Za-z]+$/.test(inputVal)) {
         result.innerHTML = `Please, type only letters!`
         result.style.display  = 'block';
-        result.style.backgroundColor = 'rgba(205, 142, 174, 0.618)';
+        result.style.backgroundColor = '#c69ab09e';
     }
     else if (start===flip) {
         result.innerHTML = `${inputVal.toUpperCase()} - is a palindrome`
         result.style.display  = 'block';
-        result.style.backgroundColor = 'rgba(129, 156, 140, 0.508)';
+        result.style.backgroundColor = '#5fb58282';
     } else {
         result.innerHTML = `${inputVal.toUpperCase()} - is not a palindrome`
         result.style.display  = 'block';
-        result.style.backgroundColor = 'rgba(205, 142, 174, 0.618)';
+        result.style.backgroundColor = '#c69ab09e';
     }
 }
